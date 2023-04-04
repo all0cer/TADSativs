434974cd080290236b0391d2179116045c4800d7
Italo Almeida <73179407+all0cer@users.noreply.github.com>
2023-04-04 16:50:29 -0300

    Update ativ5incompleto.asm

--- MIPSativmath/ativ5incompleto.asm
+++ MIPSativmath/ativ5incompleto.asm
@@ -4,17 +4,22 @@ main: add $2, $0, 5
       syscall
       add $8, $0, $2
 
-unidade: add $5, $0, 1
-         div $8, $5
-         mfhi $9
          
 centena: add $6, $0, 100
 	div $8, $6
-	mfhi $10
+	mflo $10
 	
-dezena: add $7, $0, 10
+
+	
+unidade: add $7, $0, 10
          div $8, $7
-	 mfhi $11
+	 mfhi $11 	#unidade
+dezena:	 mflo $9
+	 div  $9, $7
+	 mfhi $9
+	 
+
+	 
 	 
 soma: add $12, $10, $11
       add $13, $12, $9
@@ -25,5 +30,3 @@ mostrar: add $4, $0, $13
          
 encerrar: add $2, $0, 10
           syscall
-          
-          		
