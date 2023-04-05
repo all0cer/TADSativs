00e517220734a3b1c80cfc856563ba62d09340dc
Italo Almeida Nascimento <20222014040016@ifrn.local>
2023-04-05 17:15:09 -0300

    codigo das cedulas padrão

--- learnc#/Program.cs
+++ learnc#/Program.cs
@@ -87,16 +87,22 @@ namespace Nova_pasta
             Console.WriteLine($"{distancia:0.0000}");
         }
         static void cedulas(){
-            long valor = long.Parse(Console.ReadLine());
-            long cont = 0;
-            cont = valor/100
-            Console.WriteLine($"{cont} nota(s) de R$ 100,00");
-            Console.WriteLine($"{valor} nota(s) de R$ 50,00");
-            Console.WriteLine($"{valor} nota(s) de R$ 20,00");
-            Console.WriteLine($"{valor} nota(s) de R$ 10,00");
-            Console.WriteLine($"{valor} nota(s) de R$ 5,00");
-            Console.WriteLine($"{valor} nota(s) de R$ 2,00");
-            Console.WriteLine($"{valor} nota(s) de R$ 1,00");
+            int valor = int.Parse(Console.ReadLine());
+  
+            int nota100 = valor/100;
+            int nota50 = (valor%100)/50;
+            int nota20 = (valor%50)/20;
+            int nota10 = (valor%20)/10;
+            int nota5 = (valor%10)/5;
+            int nota2 = (valor%5)/2;
+            int nota1 = (valor%2)/1;
+            Console.WriteLine($"{nota100} nota(s) de R$ 100,00");
+            Console.WriteLine($"{nota50} nota(s) de R$ 50,00");
+            Console.WriteLine($"{nota20} nota(s) de R$ 20,00");
+           Console.WriteLine($"{nota10} nota(s) de R$ 10,00");
+            Console.WriteLine($"{nota5} nota(s) de R$ 5,00");
+            Console.WriteLine($"{nota2} nota(s) de R$ 2,00");
+            Console.WriteLine($"{nota1} nota(s) de R$ 1,00");
         }
     }
 }
