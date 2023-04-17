15626a73dc471925db13db3f0e962a34e22cc762
Italo Almeida Nascimento <20222014040016@ifrn.local>
2023-04-17 15:57:04 -0300

    Começo de estudo a POO

---
+++ learnc#/POO/calculoCirculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            circulo x = new circulo();
+            x.raio = 13;
+            Console.WriteLine($"circunferencia: {x.circunferencia()}");
+            Console.WriteLine($"area do circulo: {x.AreaCirculo()}");
+        }
+    }
+}
+
+class circulo{
+    public double raio;
+    public double AreaCirculo(){
+        return 3.14 * (raio * raio);
+    }
+    public double circunferencia(){
+        return 2*3.14*raio;
+    }
+}
