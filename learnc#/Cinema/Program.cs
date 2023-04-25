0dc1947ab4003e0fe0cb7b67dc5a5987f36654c5
all0cer <italoalmeinasc@gmail.com>
2023-04-25 08:23:40 -0300

    Reorganizando questao Cinema C#

--- learnc#/Cinema/Program.cs
+++ learnc#/Cinema/Program.cs
@@ -8,31 +8,36 @@ namespace Cinema
         {
             Ingressos x = new Ingressos();
 
-            x.SetHorario(14);
-            x.SetDia(terca);
-            x.GetDia();
-            x.GetHorario();
+            x.SetHorario(17);
+            x.SetDia("quarta");
+            Console.WriteLine(x.GetDia());
+            Console.WriteLine(x.GetHorario());
+            Console.WriteLine(x.GetValor());
         }
     }
 }
 
 
 class Ingressos{
-    private int dia, horario, entradaNormal=16, MeiaEntrada=entradaNormal/2;
+    private double  horario, entradaNormal;
+    private string dia;
 
-    public void SetHorario(int h){
-         if (h>=17 or h==00) {
+    public void SetHorario(double h){
+         if (h>=17 || h==00) {
               horario=h; entradaNormal = entradaNormal*0.50;
         }
+        else horario=h;
     }
-
-
     public void SetDia(string d){ 
-                                  if(d = "segunda" or d = "terca" or d = "quinta"){dia = d;}
-                                  if(d ='quarta'){ dia = d; entradaNormal = MeiaEntrada, SetHorario(0);}
+                                  if(d == "segunda" || d == "terca" || d == "quinta"){dia = d;}
+                                  if(d == "quarta"){ dia = d; entradaNormal = 10;}
                                   else dia = d; entradaNormal = 20;
                                 }
-    public int GetHorario(){ return horario};
-    public int GetDia(){ return dia};
+
+
+
+    public double GetHorario(){ return horario;}
+    public string GetDia(){ return dia;}
+    public double GetValor(){return entradaNormal;}
 }
 
