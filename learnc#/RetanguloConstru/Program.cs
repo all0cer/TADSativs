cf74a71fbf4521716e6e0ea1b1c2e41a70a7698d
all0cer <italoalmeinasc@gmail.com>
2023-04-26 13:58:54 -0300

    novo metodo retangulo com construct

---
+++ learnc#/RetanguloConstru/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RetanguloConstru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
+
+
+class Retangulo{
+    private double b, h;
+
+    Retangulo(double b, double h){ if(this.b>0 && this.h>0){ this.b = b; this.h = h;}}
+    public void SetBase(double b){ if(this.b>0){this.b = b;}}
+    public void SetAltura(double h){ if(this.h>0){this.h = h;}}
+
+    public double GetBase(){ return b;}
+    public double GetAltura(){ return h;}
+
+
+    public double CalcArea(){ return (b*h);}
+    public double CalcDiagonal(){ return Math.Sqrt((b*b) + (h*h));}
+
+    public override string ToString(){return $"Base = {b} Altura = {h}";}
+
+
+
+
+}
