18a72ed77c4e238c77b80313b0768cdce69ada93
all0cer <italoalmeinasc@gmail.com>
2023-04-29 13:31:17 -0300

    correcao prov1

--- learnc#/provaSem1/Program.cs
+++ learnc#/provaSem1/Program.cs
@@ -6,86 +6,79 @@ namespace provaSem1
     {
         static void Main(string[] args)
         {
-            Pais paises = new Pais[10];
-            for(int i=0; i<10; i++){
+            Pais[] paises = new Pais[10];
+            for(int i=0; i<2; i++){
                 Console.WriteLine($"Escreva o nome, populacao e área do {i} país:\n");
-                paises[i] = new Pais(Console.ReadLine(),  int.Parse(Console.ReadLine()),  double.Parse(Console.ReadLine()));
+                string n = Console.ReadLine();
+                int p = int.Parse(Console.ReadLine());
+                double a = double.Parse(Console.ReadLine());;
+                paises[i] = new Pais(n,p,a);
             }
-
-
-
-        }
-    }
-}
-
-
-
-class Pais{
-    //atributos
-    private string nome;
-    private int populacao;
-    private double area;
-
-
-    Pais(string nome, int populacao, double area){ //construtor
-        if (this.nome!=null){
-        this.nome = nome;
-        }
-        if (this.populacao>0){
-            this.populacao = populacao;
-        }
-        if (this.area>0){
-            this.area = area;
-    }
-    }
-
-
-
-
-    static void SetNome(string n){
-         if (n!=null){
-        this.nome = n;
+            Console.WriteLine(paises[0]);
         }
     }
 
-    static void SetPopulacao(int p){
-        if (p>0){
-            this.populacao = p;
+    class Pais{
+        //atributos
+        private string nome;
+        private int populacao;
+        private double area;
+
+
+        public Pais(string nome, int populacao, double area){ //construtor
+            if (nome!=null){
+                this.nome = nome;
+            }
+            if (populacao>0){
+                this.populacao = populacao;
+            }
+            if (area>0){
+                this.area = area;
+            }
         }
 
-    }
 
-    static void SetArea(double a){
-         if (a>0){
-        this.area = area;
+
+
+        public void SetNome(string n){
+            if (n!=null){
+                this.nome = n;
+            }
+        }
+
+        public void SetPopulacao(int p){
+            if (p>0){
+                this.populacao = p;
+            }
+        }
+
+        public void SetArea(double a){
+            if (a>0){
+                this.area = a;
+            }
+        }
+
+        public string GetNome(){
+            return nome;
+        }
+
+        public int GetPopulacao(){
+            return populacao;
+        }
+
+        public double GetArea(){
+            return area;
+        }
+
+        public double Densidade(){
+            return populacao/area;
+        }
+
+        public override string ToString(){
+            if(nome!=null && area>0 && populacao>0){
+                return $"Nome = {nome}\nPopulacao = {populacao}\nArea = {area}";
+            }
+            return "";
         }
     }
-
-
-
-
-    public string GetNome(){
-        return nome;
-    }
-
-    public int GetPopulacao(){
-        return populacao;
-    }
-
-    public double GetArea(){
-        return area;
-    }
-
-    public double Densidade(){
-        return populacao/area;
-    }
-
-
-    public override string ToString(){
-        if(nome!=null && area>0 && populacao>0){
-            return $"Nome = {nome}\nPopulacao = {populacao}\nArea = {area}";}
-    }
-
-
-
 }
