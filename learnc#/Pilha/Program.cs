e5fa0fe055f2504d1e3b11ddd0e39017934b7b0b
all0cer <italoalmeinasc@gmail.com>
2023-06-01 17:24:32 -0300

    config Pilha

--- learnc#/Pilha/Program.cs
+++ learnc#/Pilha/Program.cs
@@ -6,32 +6,34 @@ namespace Pilha
         static void Main(string[] args)
         {
             PilhaGenerica<string> x = new PilhaGenerica<string>();
-            x.Push("Macaco");
-            Console.WriteLine(x);
+            x.Push("camaco");
+            x.Push("Cojura");
+            x.Push("Riçoneronte");
+            Console.WriteLine(x.Peek());
         }
     }
 
     class PilhaGenerica<T>{
         private T[] objs = new T[10];
-        private int k=0;
+        private int k=0; //Contar quantos elementos tem na pilha
         public int Count{
             get {return k;}
         }
-        public void Push(T obj){
+        public void Push(T obj){ // Adicionar elemento na pilha
             if(k == objs.Length){
                 Array.Resize(ref objs, 2 * objs.Length);
             }
             objs[k] = obj;
             k++;
         }
-        public T Pop(){
+        public T Pop(){  // Retira um da pilha
             if (k == 0){
                 throw new InvalidOperationException();
             }
             k--;
             return objs[k];
         }
-        public T Peek(){
+        public T Peek(){ //Retorna o ultimo da pilha da pilha
             if (k == 0){
                 throw new InvalidOperationException();
             }
@@ -41,6 +43,5 @@ namespace Pilha
         public void Clear(){ //Não apagar os existentes, somente declarar que não há mais 
             k = 0;
         }
-
     }
 }
