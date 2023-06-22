86b4ccec7a37433644efa1bf2ccfda14dae6ad01
all0cer <italoalmeinasc@gmail.com>
2023-06-22 14:29:20 -0300

    backtracking

---
+++ algoritmosC/bk.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+/*Recebe o tamanho do labirinto(matriz até 20 por 20)
+   Recebe qtd de linhas e colunas
+   recebe os valores X e Y da célula da matriz*/
+int labirinto_bt(int labirinto[20][20],
+    int linha, int coluna,
+    int x, int y){
+    if (x==-1 || y==-1 || x==coluna || y==linha || labirinto[y][x]!=0) //Se for do tamanho da matriz, valores negativos, ou a posição das células for diferente de 0, retorna Falso
+        return 0;
+
+    if (x==coluna-1 && y==linha-1) // Se for a ultima célula da matriz, retorna verdadeiro
+        return 1;
+
+    labirinto[y][x] = 9; // Marca todas as células que passou com o valor 9
+    int ans = 0 ;
+
+    ans = labirinto_bt(labirinto, linha, coluna, x, y+1) ||
+          labirinto_bt(labirinto, linha, coluna, x+1, y) ||
+          labirinto_bt(labirinto, linha, coluna, x-1, y) ||
+          labirinto_bt(labirinto, linha, coluna, x, y-1);
+
+    labirinto[y][x] = 0;
+    return ans;
+}
+
+int main(){
+    int linha, coluna, labirinto[20][20];
+    scanf("%d %d", &linha, &coluna);
+    for (int i=0 ; i<linha ; ++i)
+        for (int j=0 ; j<coluna ; ++j)
+            scanf("%d",&labirinto[i][j]);
+
+int saida = labirinto_bt(labirinto, linha, coluna, 0,0 );
+printf("%d\n",saida);
+
+return 0;
+}
